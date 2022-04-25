b43490703349cf4643872f3538f021330dc6fa19
vyshnavlal <65948438+vyshnavlal@users.noreply.github.com>
2022-04-26 02:09:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version-2"
+    return "Version-1"
     
 if __name__ == "__main__":
-     app_port = os.getenv('APP_PORT',"8080")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
