deed3d049836c5b5526c5dda051348f81938deb1
vyshnavlal <65948438+vyshnavlal@users.noreply.github.com>
2022-04-26 21:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1><center>Version-1</center><h1>"
+    return "<h1><center>Version-2</center><h1>"
 
 
 if __name__ == "__main__":
